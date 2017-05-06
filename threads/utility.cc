a729ef5207d76eda0e26ab6d94d37d1d17d1d048
Xiangyi Meng <xymeng16@gmail.com>
2017-05-06 14:50:27 +0800

    Try to fix the memory correuption.

--- threads/utility.cc
+++ threads/utility.cc
@@ -80,7 +80,8 @@ void
 DEBUG(COLOR color, char flag, char *format, ...)
 {
 	if (DebugIsEnabled(flag)) {
-		char *t = new char[strlen(format) + strlen(color_string[color] + strlen(color_string[NORMAL]) + 1)];
+		char *t = AllocBoundedArray(strlen(format) + \
+			strlen(color_string[color] + strlen(color_string[NORMAL]) + 1));
 		strcpy(t, color_string[color]);
 		strcat(t, format);
 		strcat(t, color_string[NORMAL]);
@@ -91,6 +92,7 @@ DEBUG(COLOR color, char flag, char *format, ...)
 		vfprintf(stdout, format, ap);
 		va_end(ap);
 		fflush(stdout);
-		delete[] t;
+		DeallocBoundedArray(t, strlen(format) + \
+			strlen(color_string[color] + strlen(color_string[NORMAL]) + 1));
 	}
 }
