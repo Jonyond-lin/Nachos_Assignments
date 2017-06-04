70a8229243a43f55abdd7dd4c1f5ae7e275a8c97
Xiangyi Meng <xymeng16@gmail.com>
2017-06-04 12:09:26 +0800

    Elevator

--- threads/Elevator.cc
+++ threads/Elevator.cc
@@ -2,88 +2,275 @@
 #include "system.h"
 #include "utility.h"
 #include <sstream>
-using namespace std;
+extern Building *building;
 
-Elevator::Elevator(char *debugName, int numFloors, int myID): m_name(debugName), m_maxFloors(numFloors), m_currentFloor(1), m_occupancy(0)
+Building::Building(char *debugname, int numFloors, int numElevators)
 {
-
-}
-
-Elevator::~Elevator()
-{
-
-}
-
-void Elevator::OpenDoors()
-{
-
-}
-
-void  Elevator::CloseDoors()
-{
-
-}
-
-void Elevator::VisitFloor(int floor)
-{
-
-}
-
-void Elevator::Enter()
-{
-
-}
-
-void Elevator::Exit()
-{
-
-}
-
-void Elevator::RequestFloor(int floor)
-{
-
-}
-
-Building::Building(char *debugName, int numFloors, int numElevators): m_name(debugName), m_numFloors(numFloors), m_numElevators(numElevators)
-{
-	DEBUG(BLUE, '3', "A building is now being constructing!");
-	m_elevator = (Elevator **)AllocBoundedArray(sizeof(Elevator *) * m_numElevators);
-	for (int i = 0; i < m_numElevators; i++)
-	{
-		m_elevator[i] = new Elevator("elevator", m_numFloors, i);
+	name = debugname;
+	elevator = (Elevator **)AllocBoundedArray(sizeof(Elevator *) * numElevators);
+	m_numElevators = numElevators;
+	for (int i = 0; i < numElevators; i++) {
+		elevator[i] = new Elevator("elevator", numFloors, i);
 	}
-	m_noElevator = new Condition("no elevator condition");
-	m_noElevatorLock = new Lock("no elevator condition lock");
+	m_noElevator = new Condition("Building: no elevator condition");
+	m_noElevatorLock = new Lock("Buidling: no elevator condition lock");
 }
 
 Building::~Building()
 {
-	for (int i = 0; i < m_numElevators; i++)
-	{
-		delete m_elevator[i];
+	for (int i = 0; i < m_numElevators; i++) {
+		delete elevator[i];
 	}
-	DeallocBoundedArray((char *)elevator, sizeof(Elevator *) * m_numElevators);
+	DeallocBoundedArray((char *)elevator, m_numElevators);
 	delete m_noElevator;
 	delete m_noElevatorLock;
 }
 
 void Building::CallUp(int fromFloor)
-{
-	//TODO: Simulate the press 
-}
+{}
 
 void Building::CallDown(int fromFloor)
-{
-
-}
+{}
 
 Elevator *Building::AwaitUp(int fromFloor)
 {
-
+	// find a proper elevator
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);
+	int i;
+	while (1) {
+		for (i = 0; i < m_numElevators; i++) {
+			if (((elevator[i]->m_status == Elevator::UP) && (fromFloor > elevator[i]->currentfloor))
+				|| elevator[i]->m_status == Elevator::STOP)
+				break;
+		}
+		if (i == m_numElevators) {
+			m_noElevatorLock->Acquire();
+			m_noElevator->Wait(m_noElevatorLock);
+			m_noElevatorLock->Release();
+		}
+		else {
+			elevator[i]->m_serConLock->Acquire();
+			elevator[i]->m_serList[fromFloor] = 1;
+			elevator[i]->m_serCon->Signal(elevator[0]->m_serConLock);
+			elevator[i]->m_serConLock->Release();
+			elevator[i]->m_barrierIn[fromFloor]->Wait();
+			if ((elevator[i]->m_status == Elevator::UP) || (elevator[i]->m_status == Elevator::STOP)) {
+				(void)interrupt->SetLevel(oldLevel);
+				return elevator[i];
+			}
+			else {
+				elevator[i]->m_barrierIn[fromFloor]->Complete();
+			}
+		}
+	}
 }
 
 Elevator *Building::AwaitDown(int fromFloor)
 {
-
+	IntStatus oldLevel = interrupt->SetLevel(IntOff);
+	int i;
+	while (1) {
+		for (i = 0; i < m_numElevators; i++) {
+			if (((elevator[i]->m_status == Elevator::DOWN) && (fromFloor < elevator[i]->currentfloor))
+				|| elevator[i]->m_status == Elevator::STOP)
+				break;
+		}
+		if (i == m_numElevators) {
+			m_noElevatorLock->Acquire();
+			m_noElevator->Wait(m_noElevatorLock);
+			m_noElevatorLock->Release();
+		}
+		else {
+			elevator[i]->m_serConLock->Acquire();
+			elevator[i]->m_serList[fromFloor] = 1;
+			elevator[i]->m_serCon->Signal(elevator[0]->m_serConLock);
+			elevator[i]->m_serConLock->Release();
+			// here is a time window, so I have to set the interrupt off.
+			elevator[i]->m_barrierIn[fromFloor]->Wait();
+			if ((elevator[i]->m_status == Elevator::DOWN) || (elevator[i]->m_status == Elevator::STOP)) {
+				(void)interrupt->SetLevel(oldLevel);
+				return elevator[i];
+			}
+			else {
+				elevator[i]->m_barrierIn[fromFloor]->Complete();
+			}
+		}
+	}
 }
 
+Elevator::Elevator(char *debugName, int numFloors, int myID) :
+	name(debugName),
+	currentfloor(0),
+	m_status(STOP)
+{
+	DEBUG('t', "construction of elevator\n");
+	m_numFloors = numFloors;
+	m_id = myID;
+	m_barrierIn = (EventBarrier **)AllocBoundedArray(sizeof(EventBarrier *) * numFloors);
+	for (int i = 0; i < numFloors; i++) {
+		m_barrierIn[i] = new EventBarrier();
+	}
+	m_barrierOut = (EventBarrier **)AllocBoundedArray(sizeof(EventBarrier *) * numFloors);
+	for (int i = 0; i < numFloors; i++) {
+		m_barrierOut[i] = new EventBarrier();
+	}
+	m_serCon = new Condition("service condition");
+	m_serConLock = new Lock("service condition lock");
+	m_serList = new int[m_numFloors];
+	for (int i = 0; i < numFloors; ++i) m_serList[i] = 0;
+}
+
+Elevator::~Elevator()
+{
+	for (int i = 0; i < m_numFloors; i++) {
+		delete m_barrierIn[i];
+		delete m_barrierOut[i];
+	}
+	DeallocBoundedArray((char *)m_barrierIn, m_numFloors);
+	DeallocBoundedArray((char *)m_barrierOut, m_numFloors);
+	delete m_serCon;
+	delete m_serConLock;
+	delete[] m_serList;
+}
+
+bool Elevator::Enter()
+{
+	occupancy++;
+	m_barrierIn[currentfloor]->Complete();
+	return true;
+}
+
+void Elevator::RequestFloor(int floor)
+{
+	// DEBUG('t', "requeset floor %d\n", floor);
+	m_serConLock->Acquire();
+	// DEBUG('t', "requeset floor %d - 2\n", floor);
+	m_serList[floor] = 1;
+	m_serCon->Signal(m_serConLock);
+	m_serConLock->Release();
+	m_barrierOut[floor]->Wait();
+}
+
+void Elevator::Exit()
+{
+	m_barrierOut[currentfloor]->Complete();
+	occupancy--;
+}
+
+void Elevator::VisitFloor(int floor)
+{
+	int sleepTime = floor > currentfloor ? (floor - currentfloor) : (currentfloor - floor);
+	DEBUG('e', "Elevator::VisitFloor: sleepTime = %d\n", sleepTime);
+	sysAlarm->Pause(sleepTime);
+	currentfloor = floor;
+	printf("%s visit floor %d.\n", name, floor);
+
+	if (m_serList[floor] == 1) {
+		m_serConLock->Acquire();
+		m_serList[floor] = 0;
+		m_serConLock->Release();
+		OpenDoors();
+		CloseDoors();
+	}
+	building->m_noElevatorLock->Acquire();
+	building->m_noElevator->Broadcast(building->m_noElevatorLock);
+	building->m_noElevatorLock->Release();
+}
+
+void Elevator::OpenDoors()
+{
+	printf("%s open door.\n", name);
+	// printf("%d riders will go out\n", m_barrierOut[currentfloor]->Waiters());
+	if (m_barrierOut[currentfloor]->Waiters() > 0)
+		m_barrierOut[currentfloor]->Signal();
+	// printf("%d riders will go in\n", m_barrierIn[currentfloor]->Waiters());
+	if (m_barrierIn[currentfloor]->Waiters() > 0)
+		m_barrierIn[currentfloor]->Signal();
+}
+
+void Elevator::CloseDoors()
+{
+	printf("%s close door.\n", name);
+	//m_barrierOut[currentfloor]->Complete();
+	//m_barrierIn[currentfloor]->Complete();
+}
+
+void Elevator::Run()
+{
+	currentfloor = 0;
+	bool hasService;
+	int i;
+	int nextFloor = -1;
+	while (1) {
+		hasService = false;
+		m_serConLock->Acquire();
+		// for (int i = 0; i < m_numFloors; i++) {
+		// 	DEBUG('a', "%d, ", m_serList[i]);
+		// }
+		// DEBUG('a', "\n");
+		switch (m_status) {
+		case UP:
+			for (i = currentfloor; i < m_numFloors; i++) {
+				if (m_serList[i] == 1) {
+					hasService = true;
+					nextFloor = i;
+				}
+			}
+			if (hasService == false) {
+				for (i = currentfloor; i >= 0; i--) {
+					if (m_serList[i] == 1) {
+						hasService = true;
+						nextFloor = i;
+					}
+				}
+			}
+			break;
+		case DOWN:
+			for (i = currentfloor; i >= 0; i--) {
+				if (m_serList[i] == 1) {
+					hasService = true;
+					nextFloor = i;
+				}
+			}
+			if (hasService == false) {
+				for (i = currentfloor; i < m_numFloors; i++) {
+					if (m_serList[i] == 1) {
+						hasService = true;
+						nextFloor = i;
+					}
+				}
+			}
+			break;
+		case STOP:
+			for (i = 0; i < m_numFloors; i++) {
+				if (m_serList[i] == 1) {
+					hasService = true;
+					nextFloor = i;
+					break;
+				}
+			}
+			break;
+		}
+		if (hasService == false) {
+			nextFloor = -1;
+			m_status = STOP;
+			printf("There is no request now, so elevator %s stops on the %dth floor.\n", name, currentfloor);
+			m_serCon->Wait(m_serConLock);
+			DEBUG('t', "elevator thread wake up\n");
+		}
+		m_serConLock->Release();
+		DEBUG('t', "elevator thread wake up2\n");
+		DEBUG('t', "nextFloor: %d\n", nextFloor);
+		if (nextFloor != -1) {
+			if (nextFloor > currentfloor) {
+				m_status = UP;
+				VisitFloor(currentfloor + 1);
+			}
+			else if (nextFloor < currentfloor) {
+				m_status = DOWN;
+				VisitFloor(currentfloor - 1);
+			}
+			else VisitFloor(currentfloor);
+		}
+	}
+}
--- threads/Elevator.h
+++ threads/Elevator.h
@@ -1,91 +1,73 @@
-/*
+#ifndef ELEVATOR_H_
+#define ELEVATOR_H_
 
+#include "EventBarrier.h"
+/*
 Here are the method signatures for the Elevator and Building classes.
 You should feel free to add to these classes, but do not change the
 existing interfaces.
-
 */
-#include "synch.h"
 
 class Elevator {
-   public:
-     Elevator(char *debugName, int numFloors, int myID);
-     ~Elevator();
-     char *getName() { return m_name; }
-   
-     // elevator control interface: called by Elevator thread
-     void OpenDoors();                //   signal exiters and enterers to action
-     void CloseDoors();               //   after exiters are out and enterers are in
-     void VisitFloor(int floor);      //   go to a particular floor
-   
-     // elevator rider interface (part 1): called by rider threads.
-     void Enter();                    //   get in
-     void Exit();                     //   get out (iff destinationFloor)
-     void RequestFloor(int floor);    //   tell the elevator our destinationFloor
-   
-     // insert your methods here, if needed
+	friend class Building;
+public:
+	Elevator(char *debugName, int numFloors, int myID);
+	~Elevator();
+	char *getName() { return name; }
 
-   private:
-     char *m_name;
-   
-     int m_currentFloor;           // floor where currently stopped
-     int m_occupancy;              // how many riders currently onboard
-   
-     // insert your data structures here, if needed
-	 int m_maxFloors;
+	// elevator control interface: called by Elevator thread
+	void OpenDoors();                //   signal exiters and enterers to action
+	void CloseDoors();               //   after exiters are out and enterers are in
+	void VisitFloor(int floor);      //   go to a particular floor
+
+									 // elevator rider interface (part 1): called by rider threads.
+	bool Enter();                    //   get in
+	void Exit();                     //   get out (iff destinationFloor)
+	void RequestFloor(int floor);    //   tell the elevator our destinationFloor
+
+	void Run();
+	// insert your methods here, if needed
+
+private:
+	char *name;
+
+	int currentfloor;           // floor where currently stopped
+	int occupancy;              // how many riders currently onboard
+	int m_id;
+	int m_numFloors;
+	EventBarrier **m_barrierIn;
+	EventBarrier **m_barrierOut;
+	enum { UP, DOWN, STOP } m_status;
+
+	Condition *m_serCon; // the condition that the service list is empty
+	Lock *m_serConLock;  // the lock to be used with m_serCon also 
+						 // protects the m_serList.
+	int *m_serList;      // the service list, the ith item is 1 
+						 // means that there is a request on the ith floor
+						 // insert your data structures here, if needed
 };
-   
+
 class Building {
-   public:
-     Building(char *debugName, int numFloors, int numElevators);
-     ~Building();
-     char *getName() { return m_name; }
-   
-   				
-     // elevator rider interface (part 2): called by rider threads
-     void CallUp(int fromFloor);      //   signal an elevator we want to go up
-     void CallDown(int fromFloor);    //   ... down
-     Elevator *AwaitUp(int fromFloor); // wait for elevator arrival & going up
-     Elevator *AwaitDown(int fromFloor); // ... down
-   
-   private:
-     char *m_name;
-     Elevator **m_elevator;         // the elevators in the building (array)
-	 int m_numFloors, m_numElevators;
-     // insert your data structures here, if needed
-	 Condition *m_noElevator;
-	 Lock *m_noElevatorLock;
+	friend class Elevator;
+public:
+	Building(char *debugname, int numFloors, int numElevators);
+	~Building();
+	char *getName() { return name; }
+
+
+	// elevator rider interface (part 2): called by rider threads
+	void CallUp(int fromFloor);      //   signal an elevator we want to go up
+	void CallDown(int fromFloor);    //   ... down
+	Elevator *AwaitUp(int fromFloor); // wait for elevator arrival & going up
+	Elevator *AwaitDown(int fromFloor); // ... down
+	Elevator *GetElevator(int index) { return elevator[index]; }
+private:
+	char *name;
+	Elevator **elevator;         // the elevators in the building (array)
+	int m_numElevators;
+	Condition *m_noElevator;
+	Lock *m_noElevatorLock;
+	// insert your data structures here, if needed
 };
 
-    //here's a sample portion of a rider thread body showing how we
-    //expect things to be called.
-   
-    void rider(int id, int srcFloor, int dstFloor) {
-       Elevator *e;
-     
-       if (srcFloor == dstFloor)
-          return;
-     
-       DEBUG('t',"Rider %d travelling from %d to %d\n",id,srcFloor,dstFloor);
-       do {
-          if (srcFloor < dstFloor) {
-             DEBUG('t', "Rider %d CallUp(%d)\n", id, srcFloor);
-             building->CallUp(srcFloor);
-             DEBUG('t', "Rider %d AwaitUp(%d)\n", id, srcFloor);
-             e = building->AwaitUp(srcFloor);
-          } else {
-             DEBUG('t', "Rider %d CallDown(%d)\n", id, srcFloor);
-             building->CallDown(srcFloor);
-             DEBUG('t', "Rider %d AwaitDown(%d)\n", id, srcFloor);
-             e = building->AwaitDown(srcFloor);
-          }
-          DEBUG('t', "Rider %d Enter()\n", id);
-       } while (!e->Enter()); // elevator might be full!
-     
-       DEBUG('t', "Rider %d RequestFloor(%d)\n", id, dstFloor);
-       e->RequestFloor(dstFloor); // doesn't return until arrival
-       DEBUG('t', "Rider %d Exit()\n", id);
-       e->Exit();
-       DEBUG('t', "Rider %d finished\n", id);
-    }
-
+#endif /* ELEVATOR_H_ */
